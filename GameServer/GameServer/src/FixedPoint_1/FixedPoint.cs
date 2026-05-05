c7598d9e8f4c72f23e9d4084c8d5fa4f84819667
chuer <leatherlove001@gmail.com>
2026-05-05 11:57:25 +0800

    定点数运算符重载

--- GameServer/GameServer/src/FixedPoint_1/FixedPoint.cs
+++ GameServer/GameServer/src/FixedPoint_1/FixedPoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace ChuFixedPoint
 {
@@ -7,7 +8,40 @@ namespace ChuFixedPoint
     {
         public long raw;
         
-        public const int Multiplier = 100000;
+        public const int Multiplier = 1<<Precision;//1 << 16  = 65536
+        /// <summary>
+        /// 小数的位数
+        /// </summary>
+        public const int Precision = 16;
+        
+        public const int TotalBitCount = 64;
+        
+        public const int IntBitCount = TotalBitCount - Precision;//48
+        /// <summary>
+        /// 小数掩码
+        /// </summary>
+        public const long DecimalsMask = (long)(ulong.MaxValue >> IntBitCount);//2 ^ 16 - 1 = 65535
+
+        /// <summary>
+        /// 整数部分掩码 -1L表示所有位都是1(补码表示法) 
+        /// </summary>
+        public const long IntMask = (-1L & ~DecimalsMask);
+        /// <summary>
+        /// 小数部分取值范围 0 ~ 65536
+        /// </summary>
+        public const long DecimalsRange = DecimalsMask + 1;//2^16 
+
+        /// <summary>
+        ///  long 的范围右移16位   去掉小数部分，只保留整数部分的范围
+        /// </summary>
+        public const long MinValue = long.MinValue >> Precision;
+        public const long MaxValue = long.MaxValue >> Precision;
+                
+        public static FixedPoint Zero = new FixedPoint(0);
+        public static FixedPoint One = new FixedPoint(1);
+        public static FixedPoint Pi = new FixedPoint(3.1416f);
+        public static FixedPoint Pi2 = Pi * 2;
+        public static FixedPoint One_Div_Pi2 = 1 / Pi2;
         
         private FixedPoint(long raw)
         {
@@ -16,10 +50,69 @@ namespace ChuFixedPoint
 
         public FixedPoint(int v)
         {
-            raw = v * Multiplier;
+            raw = v * Multiplier; //乘以 2^16 相当于左移16位
+        }
+        
+        public FixedPoint(float v)
+        {
+            raw = (long)Math.Round(v * Multiplier);
+        }
+
+        #region 运算符重载
+
+        
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FixedPoint operator +(FixedPoint a, FixedPoint b)
+        {
+            a.raw+=b.raw;
+            return a;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FixedPoint operator +(FixedPoint a, int b)
+        {
+            a.raw+= (long)b << Precision;
+            return a;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FixedPoint operator +(int a, FixedPoint b)
+        {
+            return b + a;
         }
         
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FixedPoint operator -(FixedPoint a, FixedPoint b)
+        {
+            a.raw-=b.raw;
+            return a;
+        }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FixedPoint operator -(FixedPoint a, int b)
+        {
+            a.raw-= (long)b << Precision;
+            return a;
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FixedPoint operator -(int a, FixedPoint b)
+        {
+            return new FixedPoint(a) - b;
+        }
+        
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static FixedPoint operator *(FixedPoint a, FixedPoint b)
+        {
+            
+            
+            return FixedPoint.One;
+        }
+        
+        #endregion
+                
     }
 }
--- GameServer/GameServer/src/GameServer/Program.cs
+++ GameServer/GameServer/src/GameServer/Program.cs
@@ -5,8 +5,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        FixedPoint fp = new FixedPoint();
+        string maskStr =  Convert.ToString(FixedPoint.DecimalsMask, 2).PadLeft(64, '0');
+        Console.WriteLine("掩码"+FixedPoint.DecimalsMask.ToString("B24").PadLeft(64,'0'));
+        Console.WriteLine("掩码"+maskStr);
+        Console.WriteLine("ulong.MaxValue 2进制:"+ulong.MaxValue.ToString("B").PadLeft(64,'0'));
+        
+        FixedPoint fp = new FixedPoint(1);//65536
+        Console.WriteLine($"fp 2进制 =  {fp.raw.ToString("B").PadLeft(64,'0')} fp.raw = {fp.raw}");
+        
+        
+        
         
     }
 }
